68aa49a1290f0483dbad9f039893c279290d1b18
Brahim Ghaouthi <abraham.ght@hotmail.com>
2022-05-17 07:19:04 +0100

    add board logic

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
+import numpy as np
 
 
+#screen
 WIDTH, HEIGHT = 600, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Tic Tac Toe')
@@ -8,12 +10,27 @@ BG_COLOR = (28, 170, 156)
 LINE_COLOR = (23, 145, 135)
 WIN.fill(BG_COLOR)
 
+#board
+board = np.zeros((3, 3))
+
 def draw_line():
     pygame.draw.line(WIN, LINE_COLOR, (0, 200), (600, 200), 10)
     pygame.draw.line(WIN, LINE_COLOR, (0, 400), (600, 400), 10)
     pygame.draw.line(WIN, LINE_COLOR, (200, 0), (200, 600), 10)
     pygame.draw.line(WIN, LINE_COLOR, (400, 0), (400, 600), 10)
 
+def mark_square(row, col, player):
+    board[row][col] = player
+
+def is_available(row, col):
+    return board[row][col] == 0
+
+def is_full():
+    for row in range(3):
+        for col in range(3):
+            if board[row][col] == 0:
+                return False
+    return True
 
 def main():
     clock = pygame.time.Clock()
